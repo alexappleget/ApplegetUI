aafdd302752ebf48a354abdf03dfa66233cbfd16
blumed <cullan.luther@gmail.com>
2024-05-30 00:37:58 -0500

    cleaning some spacing up

--- frontend/src/components/Bacon/index.jsx
+++ frontend/src/components/Bacon/index.jsx
@@ -3,15 +3,13 @@ import Popover from 'components/+partials/Popover';
 import { styles } from 'components/Bacon/styles';
 
 export default function Bacon() {
-    const target ="bacon";
+    const target = 'bacon';
 
     return (
         <Fragment>
-            <button type="button" className="button button-bacon" popovertarget={target}>
-                Button
-            </button>
-            <Popover code={styles()} element={target} />
-            <style>{styles()}</style>
+            <button type="button" className="button button-bacon" popovertarget={ target }>Button</button>
+            <Popover code={ styles() } element={ target }  />
+            <style>{ styles() }</style>
         </Fragment>
     )
 }
